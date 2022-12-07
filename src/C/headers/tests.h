d9aff7232fca697d4ef9ba5e1f34a88e7c34ab00
Santiago Libonati <santiago.libonati@gmail.com>
2022-12-07 16:39:43 -0300

    makefile funcionando

---
+++ src/C/headers/tests.h
@@ -0,0 +1,23 @@
+#ifndef __TESTS_H__
+#define __TESTS_H__
+
+#include <assert.h>
+#include "funciones.h"
+
+void test_analizarPartida();
+
+void test_procesarArchivo();
+
+void test_leerNombre();
+
+void test_colorInicial();
+
+void test_validarJugada();
+
+void test_validarLinea();
+
+void test_casillaEnRango();
+
+void test_movimientoDisponible();
+
+#endif /* __TESTS_H__ */
